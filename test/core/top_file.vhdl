21055112c839862d1e4970b3505ae0d6ad0047ff
Rodrigo Alejandro Melo <rmelo@inti.gob.ar>
2016-05-18 17:26:08 -0300

    test/core: added a very simple VHDL core for test purposes

---
+++ test/core/top_file.vhdl
@@ -0,0 +1,25 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+library LIB_NAME;
+use LIB_NAME.PACKAGE_NAME.all;
+
+entity TOP_NAME is
+   generic (
+      FREQUENCY : positive:=25e6;
+      SECONDS   : positive:=1
+   );
+   port (
+      clk_i :  in std_logic;
+      rst_i :  in std_logic;
+      led_o : out std_logic
+   );
+end entity TOP_NAME;
+
+architecture Structural of TOP_NAME is
+begin
+
+   dut: CORE_NAME
+      generic map (FREQUENCY => FREQUENCY, SECONDS => SECONDS)
+      port map (clk_i => clk_i, rst_i => rst_i, led_o => led_o);
+
+end architecture Structural;
