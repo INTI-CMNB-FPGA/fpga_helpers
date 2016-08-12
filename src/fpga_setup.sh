5912d8a453e1233e1d24f4979562aed7d3fa3f6a
Rodrigo Alejandro Melo <rmelo@inti.gob.ar>
2016-08-12 13:42:53 -0300

    src: simplified options of fpga_setup.sh.

--- src/fpga_setup.sh
+++ src/fpga_setup.sh
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+###############################################################################
 
 if [ -f $HOME/.fpga_helpers ]; then
    source $HOME/.fpga_helpers
@@ -38,180 +39,173 @@ fi
 
 ###############################################################################
 
-OPTION=$1
-
-HELP=0
-CONFIG=0
-SETTING=0
-
-if [ $# -ne 1 ]; then
-   SETTING=1
-   echo "Select what TOOL Suite must be set:"
-   echo "0. All the available"
-   echo "1. ISE (Xilinx)"
-   echo "2. Vivado (Xilinx)"
-   echo "3. Quartus2 (Altera)"
-   echo "4. Libero-SoC (Microsemi)"
+if [ $# -eq 0 ]; then # running interactively
+   echo "c. configurations related with vendors tools"
+   echo "0. set to use all the available vendors tools"
+   echo "1. set to use ISE (Xilinx)"
+   echo "2. set to use Vivado (Xilinx)"
+   echo "3. set to use Quartus2 (Altera)"
+   echo "4. set to use Libero-SoC (Microsemi)"
    # Add here new tool code
    read -n 1 -s OPTION
    case "$OPTION" in
-      0) OPTION="--all";;
-      1) OPTION="--ise";;
-      2) OPTION="--vivado";;
-      3) OPTION="--quartus2";;
-      4) OPTION="--libero-soc";;
+      c)
+         CONFIG=1;;
+      0)
+         ISE=1
+         VIVADO=1
+         QUARTUS2=1
+         LIBEROSOC=1;;
+      1)
+         ISE=1;;
+      2)
+         VIVADO=1;;
+      3)
+         QUARTUS2=1;;
+      4)
+         LIBEROSOC=1;;
       # Add here new tool code
-      *) OPTION="--none";;
+      *)
+         echo "ERROR: '$OPTION' is not a valid option."
+         HELP=1
    esac
-elif [ $OPTION == "--help" ]; then
+elif [ $# -eq 1 ]; then # running with a option
+   OPTION=$1
+   if [ $OPTION == "--help" ]; then
+      HELP=1
+   elif [ $OPTION == "--config" ]; then
+      CONFIG=1
+   elif [ $OPTION == "--all" ]; then
+      ISE=1
+      VIVADO=1
+      QUARTUS2=1
+      LIBEROSOC=1
+   elif [ $OPTION == "--ise" ]; then
+      ISE=1
+   elif [ $OPTION == "--vivado" ]; then
+      VIVADO=1
+   elif [ $OPTION == "--quartus2" ]; then
+      QUARTUS2=1
+   elif [ $OPTION == "--libero-soc" ]; then
+      LIBEROSOC=1
+   # Add here new tool code
+   else
+      echo "ERROR: '$OPTION' is not a valid option."
+      HELP=1
+   fi
+else # running with more than a option
+   echo "ERROR: too many parameters."
    HELP=1
+fi
+
+if [[ $HELP ]]; then
    echo "FPGA Setup Help:"
-   echo "* All:"
-   echo "  --config-all"
-   echo "  --all"
-elif [[ $OPTION =~ .*config.* ]]; then
-   CONFIG=1
-else
-   SETTING=1
+   echo "* Execute fpga_setup without arguments to run interactively."
+   echo "* Available options when run with arguments (choose only one):"
+   echo "--config     : configurations related with vendors tools"
+   echo "--all        : set to use all the available vendors tools"
+   echo "--ise        : set to use ISE (Xilinx)"
+   echo "--vivado     : set to use Vivado (Xilinx)"
+   echo "--quartus2   : set to use Quartus2 (Altera)"
+   echo "--libero-soc : set to use Libero-SoC (Microsemi)"
+   # Add here new tool code
+fi
+
+if [[ $CONFIG ]]; then
+   echo "NOTE: you can use 'tab' to autocomplete options for paths and files"
 fi
 
 ###############################################################################
 # ISE                                                                         #
 ###############################################################################
 
-if [ $HELP -gt 0 ]; then
-   echo "* ISE:"
-   echo "  --config-ise"
-   echo "  --ise"
+if [[ $CONFIG ]]; then
+   echo "* Configure Xilinx ISE:"
+   read -e -p "ISE ROOT DIR:                 " -i $ISE_ROOT_DIR ISE_ROOT_DIR
+   read -e -p "ISE 64 BITS (0|1):            " -i $ISE_64_BITS  ISE_64_BITS
+   until [ $ISE_64_BITS == 0 ] || [ $ISE_64_BITS == 1 ]
+      do read -e -p "ISE 64 BITS (0|1):            " -i $ISE_64_BITS ISE_64_BITS; done
 fi
-if [ $CONFIG -gt 0 ]; then
-   if [ $OPTION == "--config-all" ] || [ $OPTION == "--config-ise" ]; then
-      CONFIG=2
-      echo "* Configure Xilinx ISE:"
-      read -e -p "ISE ROOT DIR:                 " -i $ISE_ROOT_DIR ISE_ROOT_DIR
-      read -e -p "ISE 64 BITS (0|1):            " -i $ISE_64_BITS  ISE_64_BITS
-      until [ $ISE_64_BITS == 0 ] || [ $ISE_64_BITS == 1 ]
-         do read -e -p "ISE 64 BITS (0|1):            " -i $ISE_64_BITS ISE_64_BITS; done
-   fi
-fi
-if [ $SETTING -gt 0 ]; then
-   if [ $OPTION == "--all" ] || [ $OPTION == "--ise" ]; then
-      SETTING=2
-      echo -n "* Setting Xilinx ISE... "
-      if [ $ISE_64_BITS == 1 ]; then ISE_BITS=64; else ISE_BITS=; fi
-      ISE_BIN_DIR=${ISE_ROOT_DIR}/ISE/bin/lin${ISE_BITS}
-      export PATH=$PATH:$ISE_BIN_DIR
-      echo "Done"
-   fi
+if [[ $ISE ]]; then
+   echo -n "* Setting Xilinx ISE... "
+   if [ $ISE_64_BITS == 1 ]; then ISE_BITS=64; else ISE_BITS=; fi
+   ISE_BIN_DIR=${ISE_ROOT_DIR}/ISE/bin/lin${ISE_BITS}
+   export PATH=$PATH:$ISE_BIN_DIR
+   echo "Done"
+   SET=1
 fi
 
 ###############################################################################
 # Vivado                                                                      #
 ###############################################################################
 
-if [ $HELP -gt 0 ]; then
-   echo "* Vivado:"
-   echo "  --config-vivado"
-   echo "  --vivado"
+if [[ $CONFIG ]]; then
+   echo "* Configure Xilinx Vivado:"
+   read -e -p "VIVADO ROOT DIR:              " -i $VIVADO_ROOT_DIR VIVADO_ROOT_DIR
 fi
-if [ $CONFIG -gt 0 ]; then
-   if [ $OPTION == "--config-all" ] || [ $OPTION == "--config-vivado" ]; then
-      CONFIG=2
-      echo "* Configure Xilinx Vivado:"
-      read -e -p "VIVADO ROOT DIR:              " -i $VIVADO_ROOT_DIR VIVADO_ROOT_DIR
-   fi
-fi
-if [ $SETTING -gt 0 ]; then
-   if [ $OPTION == "--all" ] || [ $OPTION == "--vivado" ]; then
-      SETTING=2
-      echo -n "* Setting Xilinx Vivado... "
-      VIVADO_BIN_DIR=${VIVADO_ROOT_DIR}/bin
-      export PATH=$PATH:$VIVADO_BIN_DIR
-      echo "Done"
-   fi
+if [[ $VIVADO ]]; then
+   echo -n "* Setting Xilinx Vivado... "
+   VIVADO_BIN_DIR=${VIVADO_ROOT_DIR}/bin
+   export PATH=$PATH:$VIVADO_BIN_DIR
+   echo "Done"
+   SET=1
 fi
 
 ###############################################################################
 # Quartus2                                                                    #
 ###############################################################################
 
-if [ $HELP -gt 0 ]; then
-   echo "* Quartus2:"
-   echo "  --config-quartus2"
-   echo "  --quartus2"
+if [[ $CONFIG ]]; then
+   echo "* Configure Altera Quartus2:"
+   read -e -p "QUARTUS2 ROOT DIR:            " -i $QUARTUS2_ROOT_DIR QUARTUS2_ROOT_DIR
 fi
-if [ $CONFIG -gt 0 ]; then
-   if [ $OPTION == "--config-all" ] || [ $OPTION == "--config-quartus2" ]; then
-      CONFIG=2
-      echo "* Configure Altera Quartus2:"
-      read -e -p "QUARTUS2 ROOT DIR:            " -i $QUARTUS2_ROOT_DIR QUARTUS2_ROOT_DIR
-   fi
-fi
-if [ $SETTING -gt 0 ]; then
-   if [ $OPTION == "--all" ] || [ $OPTION == "--quartus2" ]; then
-      SETTING=2
-      echo -n "* Setting Altera Quartus2... "
-      QUARTUS2_BIN_DIR=${QUARTUS2_ROOT_DIR}/quartus/bin
-      export PATH=$PATH:$QUARTUS2_BIN_DIR
-      echo "Done"
-   fi
+if [[ $QUARTUS2 ]]; then
+   echo -n "* Setting Altera Quartus2... "
+   QUARTUS2_BIN_DIR=${QUARTUS2_ROOT_DIR}/quartus/bin
+   export PATH=$PATH:$QUARTUS2_BIN_DIR
+   echo "Done"
+   SET=1
 fi
 
 ###############################################################################
 # Libero-SoC                                                                  #
 ###############################################################################
 
-if [ $HELP -gt 0 ]; then
-   echo "* Libero-SoC:"
-   echo "  --config-libero-soc"
-   echo "  --libero-soc"
-fi
-if [ $CONFIG -gt 0 ]; then
-   if [ $OPTION == "--config-all" ] || [ $OPTION == "--config-libero-soc" ]; then
-      CONFIG=2
-      echo "* Configure Microsemi Libero-Soc:"
-      read -e -p "LIBERO SOC ROOT DIR:          " -i $LIBEROSOC_ROOT_DIR  LIBEROSOC_ROOT_DIR
-      read -e -p "LIBERO SOC LICENSE PORT:      " -i $LIBEROSOC_LIC_PORT  LIBEROSOC_LIC_PORT
-      read -e -p "LIBERO SOC LICENSE HOST:      " -i $LIBEROSOC_LIC_HOST  LIBEROSOC_LIC_HOST
-      if [ $LIBEROSOC_LIC_HOST == "localhost" ]; then
-         read -e -p "LIBERO SOC LICENSE FILE:      " -i $LIBEROSOC_LIC_FILE  LIBEROSOC_LIC_FILE
-         read -e -p "LIBERO SOC LMGRD DIR:         " -i $LIBEROSOC_LMGRD_DIR LIBEROSOC_LMGRD_DIR
-      fi
+if [[ $CONFIG ]]; then
+   echo "* Configure Microsemi Libero-Soc:"
+   read -e -p "LIBERO SOC ROOT DIR:          " -i $LIBEROSOC_ROOT_DIR  LIBEROSOC_ROOT_DIR
+   read -e -p "LIBERO SOC LICENSE PORT:      " -i $LIBEROSOC_LIC_PORT  LIBEROSOC_LIC_PORT
+   read -e -p "LIBERO SOC LICENSE HOST:      " -i $LIBEROSOC_LIC_HOST  LIBEROSOC_LIC_HOST
+   if [ $LIBEROSOC_LIC_HOST == "localhost" ]; then
+      read -e -p "LIBERO SOC LICENSE FILE:      " -i $LIBEROSOC_LIC_FILE  LIBEROSOC_LIC_FILE
+      read -e -p "LIBERO SOC LMGRD DIR:         " -i $LIBEROSOC_LMGRD_DIR LIBEROSOC_LMGRD_DIR
    fi
 fi
-if [ $SETTING -gt 0 ]; then
-   if [ $OPTION == "--all" ] || [ $OPTION == "--libero-soc" ]; then
-      SETTING=2
-      echo -n "* Setting Microsemi Libero-SoC... "
-      LIBEROSOC_BIN_DIR=${LIBEROSOC_ROOT_DIR}/Libero/bin
-      export PATH=$PATH:$LIBEROSOC_BIN_DIR
-      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu
-      export LM_LICENSE_FILE=$LIBEROSOC_LIC_PORT@$LIBEROSOC_LIC_HOST
-      if [ $LIBEROSOC_LIC_HOST == "localhost" ]; then
-         if [ -z `pidof lmgrd` ]; then
-            echo -n "Launcingh Microsemi License manager... "
-            $LIBEROSOC_LMGRD_DIR/lmgrd -c $LIBEROSOC_LIC_FILE -l /tmp/libero-soc-license.log
-         else
-            echo -n "Microsemi License manager is already running... "
-         fi
+if [[ $LIBEROSOC ]]; then
+   echo -n "* Setting Microsemi Libero-SoC... "
+   LIBEROSOC_BIN_DIR=${LIBEROSOC_ROOT_DIR}/Libero/bin
+   export PATH=$PATH:$LIBEROSOC_BIN_DIR
+   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu
+   export LM_LICENSE_FILE=$LIBEROSOC_LIC_PORT@$LIBEROSOC_LIC_HOST
+   if [ $LIBEROSOC_LIC_HOST == "localhost" ]; then
+      if [ -z `pidof lmgrd` ]; then
+         echo -n "Launcingh Microsemi License manager... "
+         $LIBEROSOC_LMGRD_DIR/lmgrd -c $LIBEROSOC_LIC_FILE -l /tmp/libero-soc-license.log
+      else
+         echo -n "Microsemi License manager is already running... "
       fi
-      echo "Done"
    fi
+   echo "Done"
+   SET=1
 fi
 
 ###############################################################################
-# Add here new tool code                                                      #
+# Add here new tool name                                                      #
 ###############################################################################
 # Add here new tool code
-
 ###############################################################################
 
-if [ $CONFIG -eq 1 ] || [ $SETTING -eq 1 ]; then
-   echo "ERROR: unsupported option. "
-   echo "Use --help to see a list of supported options."
-fi
-
-if [ $CONFIG -eq 2 ]; then
+if [[ $CONFIG ]]; then
    echo "# FPGA Helpers Configuration file"        >  $HOME/.fpga_helpers
    echo "ISE_64_BITS=$ISE_64_BITS"                 >> $HOME/.fpga_helpers
    echo "ISE_ROOT_DIR=$ISE_ROOT_DIR"               >> $HOME/.fpga_helpers
@@ -222,10 +216,11 @@ if [ $CONFIG -eq 2 ]; then
    echo "LIBEROSOC_LIC_FILE=$LIBEROSOC_LIC_FILE"   >> $HOME/.fpga_helpers
    echo "LIBEROSOC_LIC_PORT=$LIBEROSOC_LIC_PORT"   >> $HOME/.fpga_helpers
    echo "LIBEROSOC_LIC_HOST=$LIBEROSOC_LIC_HOST"   >> $HOME/.fpga_helpers
+   echo "Configurations saved in $HOME/.fpga_helpers"
    # Add here new tool code
 fi
 
-if [ $SETTING -eq 2 ]; then
+if [[ $SET ]]; then
    echo "You are entering in a new SHELL with settings applied."
    $SHELL
 fi
