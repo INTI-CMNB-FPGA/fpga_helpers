c3a68a89dadeee93fad4a0876f8f4c0e7a17cc23
Rodrigo Alejandro Melo <rmelo@inti.gob.ar>
2016-08-12 16:48:33 -0300

    README: added some information about fpga_setup, _synt and _prog

--- data/tools/synthesis/options.tcl
+++ data/tools/synthesis/options.tcl
@@ -3,24 +3,18 @@
 #Function: fpga_file     <FILE> [-top <TOPNAME>]   Return: none
 #Constant: $FPGA_TOOL                              Name of the running tool
 
-#dev
 fpga_device "xc7a100t-3-csg324" -tool "ise"
 fpga_device "xc7a100t-3-csg324" -tool "vivado"
 fpga_device "5CGXFC7C7F23C8"    -tool "quartus2"
-#/dev
 
-#prj
-fpga_file "core_file.vhdl"    -lib "LIB_NAME"
-fpga_file "package_file.vhdl" -lib "LIB_NAME"
-fpga_file "top_file.vhdl"     -top "TOP_NAME"
-#/prj
+fpga_file "core_file.vhdl"      -lib "LIB_NAME"
+fpga_file "package_file.vhdl"   -lib "LIB_NAME"
+fpga_file "top_file.vhdl"       -top "TOP_NAME"
 
-#opt
-if {$FPGA_TOOL == "ise"} {
-   project set "FSM Encoding Algorithm" "Sequential" -process "Synthesize - XST"
-} elseif {$FPGA_TOOL == "vivado"} {
-   set_property "steps.synth_design.args.fsm_extraction" "sequential" [get_runs synth_1]
-} elseif {$FPGA_TOOL == "quartus2"} {
-   set_global_assignment -name STATE_MACHINE_PROCESSING SEQUENTIAL
-}
-#/opt
+#if {$FPGA_TOOL == "ise"} {
+#   project set "FSM Encoding Algorithm" "Sequential" -process "Synthesize - XST"
+#} elseif {$FPGA_TOOL == "vivado"} {
+#   set_property "steps.synth_design.args.fsm_extraction" "sequential" [get_runs synth_1]
+#} elseif {$FPGA_TOOL == "quartus2"} {
+#   set_global_assignment -name STATE_MACHINE_PROCESSING SEQUENTIAL
+#}
