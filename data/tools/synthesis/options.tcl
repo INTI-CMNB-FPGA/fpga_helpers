64ca39a1ed2d5382a3e169a527b9d234859a9c80
Rodrigo Alejandro Melo <rmelo@inti.gob.ar>
2017-01-11 15:43:31 -0300

    data/tools/synthesis: added libero-soc.tcl (WIP)

--- data/tools/synthesis/options.tcl
+++ data/tools/synthesis/options.tcl
@@ -6,6 +6,7 @@
 fpga_device "xc7a100t-3-csg324" -tool "ise"
 fpga_device "xc7a100t-3-csg324" -tool "vivado"
 fpga_device "5CGXFC7C7F23C8"    -tool "quartus2"
+fpga_device "M2S090TS-1-fg484"  -tool "libero-soc"
 
 fpga_file "core_file.vhdl"      -lib "LIB_NAME"
 fpga_file "package_file.vhdl"   -lib "LIB_NAME"
@@ -17,4 +18,6 @@ fpga_file "top_file.vhdl"       -top "TOP_NAME"
 #   set_property "steps.synth_design.args.fsm_extraction" "sequential" [get_runs synth_1]
 #} elseif {$FPGA_TOOL == "quartus2"} {
 #   set_global_assignment -name STATE_MACHINE_PROCESSING SEQUENTIAL
+#} elseif {$FPGA_TOOL == "libero-soc"} {
+#
 #}
