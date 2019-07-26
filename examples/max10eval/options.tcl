ccb6b4b66c92ad9eb2dc7cfcc3d6556547c83b23
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2019-07-26 20:26:01 -0300

    Moved examples/10M08 to examples/max10eval

--- examples/max10eval/options.tcl
+++ examples/max10eval/options.tcl
@@ -2,5 +2,5 @@ fpga_device "10M08SAE144C8G"
 
 fpga_file "../hdl/blinking.vhdl"     -lib "examples"
 fpga_file "../hdl/examples_pkg.vhdl" -lib "examples"
-fpga_file "10m08.vhdl"               -top "Top"
-fpga_file "10m08.tcl"
+fpga_file "max10eval.vhdl"           -top "Top"
+fpga_file "max10eval.tcl"
